91e13f3b323d9171a2719d2ac2c5f6c62a409071
MohadmadReza <mr.rahmatzade371@gmail.com>
2024-05-09 02:11:51 -0600

    manipulating front end with jquery and ajax

---
+++ asp mvc sample with course udemi/Controllers/BillsController.cs
@@ -0,0 +1,32 @@
+﻿using asp_mvc_sample_with_course_udemi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace asp_mvc_sample_with_course_udemi.Controllers
+{
+    public class BillsController : Controller
+    {
+        private readonly IItemService itemService;
+
+        public BillsController(IItemService itemService)
+        {
+            this.itemService = itemService;
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var items = await itemService.GetAll();
+
+            ViewBag.ItemsList = new SelectList(items, "Id", "Name");
+
+            return View();
+        }
+
+        public async Task<IActionResult> GetPrice(int id)
+        {
+            var item = await itemService.GetById(id);
+
+            return Json(item.Price);
+        }
+    }
+}
---
+++ asp mvc sample with course udemi/ViewModels/BillViewModel.cs
@@ -0,0 +1,21 @@
+﻿using asp_mvc_sample_with_course_udemi.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace asp_mvc_sample_with_course_udemi.ViewModels
+{
+    public class BillViewModel
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public PaymentType PaymentType { get; set; }
+        [Display(Name = "Items List")]
+        public int ItemId { get; set; }
+        //public List<SelectListItem> ItemList { get; set; }
+        public decimal ItemPrice { get; set; }
+        public int Quantity { get; set; }
+        public int Discount { get; set; }
+        public decimal Total { get; set; }
+        public decimal GrandTotal { get; set; }
+    }
+}
